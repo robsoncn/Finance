08873ddb16cb257c367c6590a4cfab2113a51b5c
Wilbert Ribeiro <wilbert@khronus.com.br>
2011-02-25 17:02:29 -0300

    Corrigindo bug de código

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,4 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-
-
-def index
-  @user = User.all
-  respond_to do |format|
-    format.html # index.html.erb
-    format.xml  { render :xml => @user }
-  end
-end
-
-def new
-  @user = User.new
-  respond_to do |format|
-    format.html # new.html.erb
-    format.xml  { render :xml => @user}
-=======
   before_filter :login_required, :except => [:new, :create]
   
   def new
@@ -25,7 +8,6 @@ def new
       format.html # new.html.erb
       format.xml  { render :xml => @user}
     end
->>>>>>> d208d9da2f6abd318c7b3607eb67c2055784e2e4
   end
 
 
@@ -36,16 +18,10 @@ def new
   def show
     @user = current_user
 
-<<<<<<< HEAD
-  respond_to do |format|
-    format.html # show.html.erb
-    format.xml  { render :xml => @user }
-=======
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @users }
     end
->>>>>>> d208d9da2f6abd318c7b3607eb67c2055784e2e4
   end
 
   def create
@@ -62,20 +38,6 @@ def new
     end
   end
 
-<<<<<<< HEAD
-def update
-  @user = User.find(params[:id])
-
-  respond_to do |format|
-    if @user.update_attributes(params[:user])
-      flash[:notice] = 'User was successfully updated.'
-      format.html { redirect_to(@user) }
-      format.xml  { head :ok }
-    else
-      format.html { render :action => "edit" }
-      format.xml  { render :xml => @user.errors,
-	                :status => :unprocessable_entity }
-=======
   def update
     @users = current_user
 
@@ -89,7 +51,6 @@ def update
         format.xml  { render :xml => @user.errors,
 	                  :status => :unprocessable_entity }
       end
->>>>>>> d208d9da2f6abd318c7b3607eb67c2055784e2e4
     end
   end
 
